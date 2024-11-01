67108552f4117cbbffd5918683dceea82c536da9
Ayush <ayushagarwal.dev@gmail.com>
2024-11-01 12:40:42 +0530

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
+import Home from "./pages/Home/Home.jsx";
+import Login from "./pages/Login/Login.jsx";
+import Profile from "./pages/Profile/Profile.jsx";
 
 const App = () => {
   return (
-    <div>
+    <BrowserRouter basename={"/"}>
       <Navbar />
-    </div>
+      <Routes>
+        <Route path={"/"} element={<Home />} />
+        <Route path={"/login"} element={<Login />} />
+        <Route path={"/profile"} element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 export default App;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,6 @@
+import Navbar from "../../components/Navbar.jsx";
+
+const Home = () => {
+  return <>Home</>;
+};
+export default Home;
