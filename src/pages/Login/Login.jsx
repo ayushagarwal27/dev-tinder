4484b6912b9a5c834997d6125ed391a00c249517
Ayush <ayushagarwal.dev@gmail.com>
2024-11-02 15:55:42 +0530

    add login form, integrate login api

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,4 +1,67 @@
+import { useState } from "react";
+import axios from "axios";
+
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleSubmit() {
+    try {
+      const data = await axios.post(
+        "http://localhost:3000/auth/login",
+        {
+          email,
+          password,
+        },
+        { withCredentials: true },
+      );
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  return (
+    <div className={"flex justify-center items-center my-10 w-full"}>
+      <div className="card bg-base-300 w-96 shadow-xl">
+        <div className="card-body">
+          <h2 className="text-center font-semibold text-[24px]">Login</h2>
+
+          <div className={"flex flex-col gap-4 my-4"}>
+            <label className="form-control w-full max-w-xs">
+              <div className="label">
+                <span className="label-text">Email</span>
+              </div>
+              <input
+                type="text"
+                placeholder="Email"
+                className="input input-bordered w-full max-w-xs"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </label>
+            <label className="form-control w-full max-w-xs">
+              <div className="label">
+                <span className="label-text">Password</span>
+              </div>
+              <input
+                type="password"
+                placeholder="*******"
+                className="input input-bordered w-full max-w-xs"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </label>
+          </div>
+
+          <div className="card-actions justify-center">
+            <button className="btn btn-primary" onClick={handleSubmit}>
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Login;
